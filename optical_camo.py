7e505c0da4b952e3019e3cdbb51f21eb209f4b50
Rod <rodjohnguillen@gmail.com>
2026-03-04 19:36:29 +0800

    Add red mask and initial bitwise logic

--- optical_camo.py
+++ optical_camo.py
@@ -5,7 +5,6 @@ import time
 cap = cv2.VideoCapture(0)
 time.sleep(3)
 background = 0
-
 for i in range(30):
     ret, background = cap.read()
 
@@ -13,9 +12,15 @@ while(cap.isOpened()):
     ret, img = cap.read()
     if not ret: break
     
-    cv2.imshow("Optical camo", img)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0, 120, 70])
+    upper_red = np.array([10, 255, 255])
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    res2 = cv2.bitwise_and(bg_image, bg_image, mask=mask)
+    
+    cv2.imshow("Optical Camo", res2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
